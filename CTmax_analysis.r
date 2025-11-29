5bf5cbbc4b06f359e28cfcc3f0465d060a3f0cab
Olaf Jensen <olaf.p.jensen@gmail.com>
2025-11-29 12:00:41 -0600

    Add CTmax data analysis script
    This script analyzes CTmax data from a research trip, fitting linear models to examine the effects of species, acclimation, and weight on CTmax. It also includes a boxplot visualization of the CTmax data by species.

---
+++ CTmax_analysis.r
@@ -0,0 +1,27 @@
+#Analysis of CTmax data from September/October 2025 research trip to Balj River, Mongolia
+#Olaf Jensen
+
+library(tidyverse)
+
+#read in data file
+data = read.csv("CTmax.csv")
+
+mod1 = lm(Ctmax_degC ~ Species, data = data)
+
+summary(mod1)
+
+mod2 = lm(Ctmax_degC ~ Acclimation, data = data)
+
+summary(mod2)
+
+mod3 = lm(Ctmax_degC ~ Weight_g, data = data)
+
+summary(mod3)
+
+#plotting
+species_boxplots <- ggplot(data = data, mapping = aes(x = Species, y = Ctmax_degC)) +
+  geom_boxplot() +
+  geom_jitter() +
+  theme_minimal()
+
+species_boxplots
